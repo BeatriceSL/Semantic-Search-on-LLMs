a03900f5c0b29615488c20bd058a7c55fa88b6d3
BeatriceSL <51354660+BeatriceSL@users.noreply.github.com>
2024-05-04 16:23:37 -0700

    initial

---
+++ Keyword-Search.py
@@ -0,0 +1,65 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+#import Weaviate to access the Wikipedia database
+import weaviate
+auth_config = weaviate.auth.AuthApiKey(
+    api_key=os.environ['WEAVIATE_API_KEY'])
+
+client = weaviate.Client(
+    url=os.environ['WEAVIATE_API_URL'],
+    auth_client_secret=auth_config,
+    additional_headers={
+        "X-Cohere-Api-Key": os.environ['COHERE_API_KEY'],
+    }
+)
+client.is_ready() 
+
+#Keyword Search
+def keyword_search(query,
+                   results_lang='en',
+                   properties = ["title","url","text"],
+                   num_results=3):
+
+    where_filter = {
+    "path": ["lang"],
+    "operator": "Equal",
+    "valueString": results_lang
+    }
+    
+    response = (
+        client.query.get("Articles", properties)
+        .with_bm25(
+            query=query
+        )
+        .with_where(where_filter)
+        .with_limit(num_results)
+        .do()
+        )
+
+    result = response['data']['Get']['Articles']
+    return result
+query = "What is the most searched food in 2022?"
+keyword_search_results = keyword_search(query)
+print(keyword_search_results)
+
+def print_result(result):
+    """ Print results with colorful formatting """
+    for i,item in enumerate(result):
+        print(f'item {i}')
+        for key in item.keys():
+            print(f"{key}:{item.get(key)}")
+            print()
+        print()
+
+print_result(keyword_search_results)
+
+query = "What is the most googled recipes in 2022?"
+keyword_search_results = keyword_search(query, results_lang='de')
+print_result(keyword_search_results)
+
+
+
+
+
